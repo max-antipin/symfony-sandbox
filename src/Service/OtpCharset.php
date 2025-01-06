76e1855451bf7dee478081436d40448405dd32c1
Max Antipin <max.v.antipin@gmail.com>
2025-01-06 16:44:34 +0300

    Update OTP generator

--- src/Service/OtpCharset.php
+++ src/Service/OtpCharset.php
@@ -5,6 +5,7 @@ namespace App\Service;
 enum OtpCharset
 {
     case NUMBER;
+    case ALPHA;
     case ALPHA_LC;
     case ALPHA_UC;
 }
--- src/Service/OtpGenerator.php
+++ src/Service/OtpGenerator.php
@@ -4,13 +4,35 @@ namespace App\Service;
 
 readonly class OtpGenerator
 {
+    public const MIN_LENGTH = 4;
+
     public function __construct(private OtpCharset $charset, private int $length)
     {
-        
+        if ($length < self::MIN_LENGTH) {
+            throw new \ValueError('Length must be greater than or equal to ' . self::MIN_LENGTH);
+        }
+        $this->chars = array_merge(range('a', 'z'), range('A', 'Z'));
     }
 
     public function __invoke(): string
     {
-        return (string)mt_rand(0, 10 ** $this->length);
+        return match ($this->charset) {
+            OtpCharset::NUMBER => sprintf("%0{$this->length}d", random_int(0, 10 ** $this->length)),
+            OtpCharset::ALPHA => $this->generateAlpha(),
+            OtpCharset::ALPHA_LC => strtolower($this->generateAlpha()),
+            OtpCharset::ALPHA_UC => strtoupper($this->generateAlpha())
+        };
     }
+
+    private function generateAlpha(): string
+    {
+        $str = '';
+        $c = count($this->chars);
+        foreach (str_split(random_bytes($this->length)) as $char) {
+            $str .= $this->chars[ord($char) % $c];
+        }
+        return $str;
+    }
+
+    private array $chars;
 }
